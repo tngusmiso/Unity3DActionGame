aa82375da6ae8406101372e2d13e8cd6b001b81b
tngusmiso <k1miso012@gmail.com>
2018-12-11 23:51:22 +0900

    Player HP bar

---
+++ ActionGame/Assets/Scripts/Manager/HpManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HpManager : MonoBehaviour {
+
+	public GameObject gameObject;
+	Image hpbar;
+	CharacterStatus status;
+	
+	void Start () {
+		status = gameObject.GetComponent<CharacterStatus>();
+		hpbar = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hpbar.fillAmount = (float)status.HP / status.MaxHP;
+	}
+
+	public void HandleBar(float HP, float Max){
+		 hpbar.fillAmount = HP/Max;
+	}
+
+	// private float Map (float value, float inMin, float inMax, float outMin,float outMax){
+	// 	return (value - inMin) * (outMax - outMin) / (inMin - inMax) + outMin;
+	// 	// (80 - 0) * (1 - 0) / (100 - 0) + 0
+	// }
+}
